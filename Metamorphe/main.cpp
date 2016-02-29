a85691d0de47ed882ab58431b43e4d305672bd30
maximeb97 <maxime.betemps@epitech.eu>
2016-02-29 17:00:57 +0100

    cleaning

--- Metamorphe/main.cpp
+++ Metamorphe/main.cpp
@@ -5,26 +5,26 @@
 DWORD			*morph_list;
 unsigned int	morph_count = 0;
 
-#define GET_EIP _asm	{								\
-						_asm push eax					\
-						_asm mov eax, [esp + 4]			\
-						_asm push ecx					\
-						_asm push edx					\
-						_asm mov edx, [morph_list]		\
-						_asm mov ecx, 0					\
-						_asm my_loop:					\
+#define GET_EIP _asm	{	\
+						_asm push eax	\
+						_asm mov eax, [esp + 4]	\
+						_asm push ecx	\
+						_asm push edx	\
+						_asm mov edx, [morph_list]	\
+						_asm mov ecx, 0	\
+						_asm my_loop:	\
 						_asm cmp eax, [edx + ecx * 4]	\
-						_asm je my_leave				\
-						_asm cmp [edx + ecx * 4], 0		\
-						_asm je my_change				\
-						_asm inc ecx					\
-						_asm jmp my_loop				\
-						_asm my_change:					\
+						_asm je my_leave	\
+						_asm cmp [edx + ecx * 4], 0	\
+						_asm je my_change	\
+						_asm inc ecx	\
+						_asm jmp my_loop	\
+						_asm my_change:	\
 						_asm mov [edx + ecx * 4], eax	\
-						_asm my_leave:					\
-						_asm pop edx					\
-						_asm pop ecx					\
-						_asm pop eax					\
+						_asm my_leave:	\
+						_asm pop edx	\
+						_asm pop ecx	\
+						_asm pop eax	\
 						}
 
 _declspec(naked) void _my_hook()
@@ -44,9 +44,8 @@ void		MainThread()
 {
 	int		morph_nb;
 
-	morph_nb = 2;
+	morph_nb = 1;
 	morph_list = (DWORD*)malloc(sizeof(DWORD) * morph_nb);
-	//spawn_metamorphe(0x00F81027, (DWORD)&_my_hook, 9);
 	//spawn_metamorphe(0x00F81027, (DWORD)&_my_hook, 9);
 	init_metamorphe();
 }
